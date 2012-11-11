599ed42769319a705ee83820fa39e0e9a7659302
abhishekes <abhishek.es@gmail.com>
2012-11-10 22:56:51 -0600

    Receiving part

--- dfs/dfs_listener.h
+++ dfs/dfs_listener.h
@@ -17,8 +17,7 @@
 #include "../ui/ui.h"
 #include "../failure_detector/topology_update.h"
 
-#define ADMISSION_CONTACT_IP "192.168.2.7"	
-#define ADMISSION_CONTACT_PORT 2000		
+
 #define DFS_LISTEN_PORT 28000
 
 void* dfs_listener(void*);
