0e5f4900db1f3b0b5d9a6a75010608fe12fcb1ec
abhishekes <abhishek.es@gmail.com>
2012-11-11 22:13:34 -0600

    Receiving part

--- failure_detector/failure_detector.c
+++ failure_detector/failure_detector.c
@@ -36,7 +36,7 @@ int node_init() {
 			pthread_create(&send_thread, NULL, heartbeat_send, (void*)0);
 			pthread_create(&receive_thread, NULL, heartbeat_receive, (void*)0);
 			pthread_create(&listen_thread, NULL, topology_update, (void*)0);
-			pthread_create(&dfs_listener_thread, &attr, dfs_listener, (void*)0);
+			pthread_create(&dfs_listener_thread, NULL, dfs_listener, (void*)0);
 
 			//printf("Threads created\n");	
 			//I still need to tell others, if there are any, that I've joined. I've told to prev and next in the join_topology()
